982ca577e0ff5d34a9d989c6bf0aca4251f0bead
Unknown <olav.telseth94@hotmail.com>
2018-03-20 18:16:32 +0100

    index.html

--- src/test/java/no/stud/ntnu/hackathon/HackathonHackathonApplicationTests.java
+++ src/test/java/no/stud/ntnu/hackathon/HackathonHackathonApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HackathonApplicationTests {
+public class HackathonHackathonApplicationTests {
 
 	@Test
 	public void contextLoads() {
