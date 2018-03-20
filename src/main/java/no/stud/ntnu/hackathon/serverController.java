0c1823f6c5ccd2b925ee030242aa885fe8e90b09
Unknown <olav.telseth94@hotmail.com>
2018-03-20 18:16:17 +0100

    Added Html

--- src/main/java/no/stud/ntnu/hackathon/serverController.java
+++ src/main/java/no/stud/ntnu/hackathon/serverController.java
@@ -1,26 +1,17 @@
 package no.stud.ntnu.hackathon;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-@RestController
+@Controller
 public class serverController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
 }
