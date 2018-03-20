7816b9dc22fbdb1446ec0bfe769c050e83e17e8b
Unknown <olav.telseth94@hotmail.com>
2018-03-20 16:42:09 +0100

    Added in the Spring boot

---
+++ src/main/java/no/stud/ntnu/hackathon/serverController.java
@@ -0,0 +1,26 @@
+package no.stud.ntnu.hackathon;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class serverController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+}
