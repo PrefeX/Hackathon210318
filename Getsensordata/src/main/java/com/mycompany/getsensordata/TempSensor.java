babc5907533badf9efa3724c0758096a200cf79f
andreas <andrrekd@stud.ntnu.no>
2018-03-21 15:54:22 +0100

    not working

--- Getsensordata/src/main/java/com/mycompany/getsensordata/TempSensor.java
+++ Getsensordata/src/main/java/com/mycompany/getsensordata/TempSensor.java
@@ -16,44 +16,31 @@ import java.net.URLEncoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.JSONObject;
-import org.json.JSONString;
 
 /**
  *
  * @author Andreas
  */
-public class testsensordata {
-
-    public static void main(String args[]) {
-        try {
-            testsensordata test = new testsensordata();
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(testsensordata.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
+public class TempSensor {
 
     private String serviceAccountKey = "banmjq324te000b24ubg";
     private String serviceAccountSecret = "03ac12bfc30047d5b7bba0cbb4d36c1f";
     private String projectId = "baj44uni50gg00dhk030";
     private String apiUrlBase = "https://api.disruptive-technologies.com/v2beta1";
     private String apiDeviceUrl = apiUrlBase + "/projects/" + projectId + "/devices";
-    private String codeExampleSensorDisplayName = "Team 1 Prox";
+    private String codeExampleSensorDisplayName = "Team 1 Temp";
+    private JSONObject JSONObject;
 
+    public TempSensor() {
 
-    public testsensordata() throws UnsupportedEncodingException {
         try {
-            String response = get(apiDeviceUrl + "?label_filters=" + URLEncoder.encode("name=" + codeExampleSensorDisplayName, "UTF-8"));
-            System.out.println(response);
-            JSONObject tempJSON = new JSONObject(response);
-            
-            JSONObject devices = (JSONObject) tempJSON.getJSONArray("devices").get(0);
-            System.out.println(devices.getString("type"));
-        } catch (IOException ex) {
-            Logger.getLogger(testsensordata.class.getName()).log(Level.SEVERE, null, ex);
+            this.JSONObject = getJSONObject(update(apiDeviceUrl + "?label_filters=" + URLEncoder.encode("name=" + codeExampleSensorDisplayName, "UTF-8"));
+        } catch (Exception ex) {
         }
+
     }
-    private String get(String urlString) throws MalformedURLException, IOException{
+
+    private String update(String urlString) throws MalformedURLException, IOException {
         URL url = new URL(urlString);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         String userpass = serviceAccountKey + ":" + serviceAccountSecret;
@@ -70,7 +57,12 @@ public class testsensordata {
         conn.disconnect();
         return response;
     }
-    private JSONObject parse(JSONString string){
+
+    public JSONObject getJSONObject(String string) {
         return new JSONObject(string);
+    }
+
+    public String getSensorType() {
+
     }
 }
