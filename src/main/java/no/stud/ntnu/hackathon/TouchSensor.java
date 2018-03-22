9a8f5a3634983571afd54fd60f9e27002aa453d8
andreas <andrrekd@stud.ntnu.no>
2018-03-22 03:32:02 +0100

    Added boolean if action required for touch sensor

--- src/main/java/no/stud/ntnu/hackathon/TouchSensor.java
+++ src/main/java/no/stud/ntnu/hackathon/TouchSensor.java
@@ -7,10 +7,14 @@ import java.io.IOException;
 
 public class TouchSensor extends Sensor {
 
+    private String lastTouch;
+    private boolean actionRequired;
+
     public TouchSensor(String sensorName) {
         super(sensorName);
     }
-    public String getTouch(){
+
+    public String getTouch() {
         String result = "";
         JSONArray devices = super.jsonObject.getJSONArray("devices");
         JSONObject device = devices.getJSONObject(0);
@@ -19,21 +23,29 @@ public class TouchSensor extends Sensor {
         String updateTime = touch.getString("updateTime");
         super.setUpdateTime(updateTime);
         result = updateTime;
-
+        if (!lastTouch.equals(result)) {
+            actionRequired = true;
+            lastTouch = result;
+        }
         //return json.toString();
         return result;
     }
-    public int getSignalStrength(){
+
+    public int getSignalStrength() {
         JSONObject devices = (JSONObject) super.jsonObject.getJSONArray("devices").get(0);
         JSONObject reported = devices.getJSONObject("reported");
         JSONObject networkStatus = reported.getJSONObject("networkStatus");
         return networkStatus.getInt("signalStrength");
     }
+    public void setActionDone(){
+        actionRequired = false;
+    }
+
     public int getBatteryStatus() {
         JSONObject devices = (JSONObject) super.jsonObject.getJSONArray("devices").get(0);
         JSONObject reported = devices.getJSONObject("reported");
         JSONObject batteryStatus = reported.getJSONObject("batteryStatus");
         return batteryStatus.getInt("percentage");
     }
-    
+
 }
